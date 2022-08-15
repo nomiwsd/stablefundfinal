abf9d05c3d8817afbbde2fd45bcd8bc71f40e15f
nomiwsd <nomimalik8051@gmail.com>
2022-08-15 15:11:31 +0500

    practise of React

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 
-function Header() {
+
+const Header = () => {
+
+ const[name, setName]=useState('Mario');
+const [age,Setage]=useState(25);
+const [blogs,setBlogs]=useState(
+  [{title:'My fisrt Website',body: 'Lorem4dsdd',author:'Nomi' ,id:1},
+  {title:'My Second Website',body: 'Lorem4dsdd',author:'Nomi' ,id:2},
+  {title:'My Third Website',body: 'Lorem4dsdd',author:'Nomi' ,id:3}]
+);
+  const handleClick = () =>{
+    setName('Nomi');
+    Setage(22);
+  }
   return (
     <div>
-      
+      <p>{name} is {age} years old</p>
+      {blogs.map((blog)=>(
+        <div className='blog=preview' key={blog.id}>
+          <h3>
+            {blog.title}
+          </h3>
+          <p>written by{
+blog.author}</p>
+        </div>
+      ))}
+      <button onClick = {handleClick}>Click Me</button>
+
     </div>
   )
 }
